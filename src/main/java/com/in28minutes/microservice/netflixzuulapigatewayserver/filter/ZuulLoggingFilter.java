f65e73e0309a1ec19fc33da2398db0c35729f2f2
kiyoung <yky2798@gmail.com>
2018-04-29 00:42:24 +0900

    second commit

---
+++ src/main/java/com/in28minutes/microservice/netflixzuulapigatewayserver/filter/ZuulLoggingFilter.java
@@ -0,0 +1,45 @@
+package com.in28minutes.microservice.netflixzuulapigatewayserver.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter{
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Override
+	public Object run() {
+		// TODO Auto-generated method stub
+		HttpServletRequest request = 
+		RequestContext.getCurrentContext().getRequest();
+		logger.info("request->{} request uri ->{}",request,request.getRequestURI());
+		
+		
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+}
